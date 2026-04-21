fd6f05d7f0e1bbbbc36f86cd7e5619e83781510c
Chetna Deshmukh <chetnadeshmukh65@gmail.com>
2026-04-21 23:07:32 +0530

    Time: 41 ms (28.21%), Space: 35.8 MB (9.97%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.c
@@ -0,0 +1,24 @@
+int compare(const void* a, const void* b) {
+    int* i1 = *(int**)a;
+    int* i2 = *(int**)b;
+    return i1[1] - i2[1];
+}
+
+int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize) {
+    if (intervalsSize == 0) return 0;
+
+    qsort(intervals, intervalsSize, sizeof(int*), compare);
+
+    int count = 0;
+    int end = intervals[0][1];
+
+    for (int i = 1; i < intervalsSize; i++) {
+        if (intervals[i][0] < end) {
+            count++;
+        } else {
+            end = intervals[i][1];
+        }
+    }
+
+    return count;
+}
